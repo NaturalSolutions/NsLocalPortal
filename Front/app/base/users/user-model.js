5dcb4ed621c868a9e31c9115529563de197d0c4f
ktalbi <khaled_talbi@hotmail.com>
2018-08-30 11:57:00 +0200

    add "gestionnaire" role

--- Front/app/base/users/user-model.js
+++ Front/app/base/users/user-model.js
@@ -1,53 +1,52 @@
 define([
-	'backbone'
+    'backbone'
 ], function(Backbone) {
 
-  'use strict';
+    'use strict';
 
-  return Backbone.Model.extend({
-    schema: {
-      name: {
-        type: 'Text',
-        title: 'Nom',
-        editorClass: 'form-control',
-        validators: ['required'],
-        inputAttrs : {'tabindex' : '1'}
-      },
-      firstName: {
-        type: 'Text',
-        title: 'Prénom',
-        editorClass: 'form-control',
-        validators: ['required'],
-        inputAttrs : {'tabindex' : '2'}
-      },
-      password: {
-        type: 'Password',
-        title: 'Mot de passe',
-        editorClass: 'form-control',
-        validators: ['required'],
-        inputAttrs : {'tabindex' : '3'}
-      },
-      confirmpassword: {
-        type: 'Password',
-        title: 'Confirmer le mot de passe',
-        editorClass: 'form-control',
-        validators: ['required'],
-        inputAttrs : {'tabindex' : '4'}
-      },
-      mail: {
-        title: 'Email',
-        editorClass: 'form-control',
-        validators: ['email'],
-        inputAttrs : {'tabindex' : '5'}
-      },
-      role : {
-        title: 'Role',
-        type: 'Select',
-        editorClass: 'form-control',
-        validators: ['required'],
-        options: [{ val: 0, label: '' }, { val: 1, label: 'Administrateur' }, { val: 3, label: 'Utilisateur' },{ val: 5, label: 'Lecteur' }]
-      }
-    }
-  });
+    return Backbone.Model.extend({
+        schema: {
+            name: {
+                type: 'Text',
+                title: 'Nom',
+                editorClass: 'form-control',
+                validators: ['required'],
+                inputAttrs: { 'tabindex': '1' }
+            },
+            firstName: {
+                type: 'Text',
+                title: 'Prénom',
+                editorClass: 'form-control',
+                validators: ['required'],
+                inputAttrs: { 'tabindex': '2' }
+            },
+            password: {
+                type: 'Password',
+                title: 'Mot de passe',
+                editorClass: 'form-control',
+                validators: ['required'],
+                inputAttrs: { 'tabindex': '3' }
+            },
+            confirmpassword: {
+                type: 'Password',
+                title: 'Confirmer le mot de passe',
+                editorClass: 'form-control',
+                validators: ['required'],
+                inputAttrs: { 'tabindex': '4' }
+            },
+            mail: {
+                title: 'Email',
+                editorClass: 'form-control',
+                validators: ['email'],
+                inputAttrs: { 'tabindex': '5' }
+            },
+            role: {
+                title: 'Role',
+                type: 'Select',
+                editorClass: 'form-control',
+                validators: ['required'],
+                options: [{ val: 0, label: '' }, { val: 1, label: 'Administrateur' }, { val: 2, label: 'Gestionnaire' }, { val: 3, label: 'Utilisateur' }]
+            }
+        }
+    });
 });
-
